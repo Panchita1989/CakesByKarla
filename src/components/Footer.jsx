d297cee815e97b2e827087ce7c327b650a11b2d1
Panchita <franciscalandwehr@hotmail.com>
2025-05-09 11:12:07 -0500

    started project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+
+
+export default function Footer(){
+    return(
+        <div className="footer">COPYRIGHT &copy; Franciska Landwehr, 2025</div>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import Logo from '../assets/images/logo.png'
+
+export  default function Header(){
+    return(
+        <header className="header">
+            <div className="spacer"></div>
+            <div className="logo">
+            <img src={Logo} alt="logo" />
+            </div>
+            <div className="socialLinks">
+                <ul>
+                <li><a href="#">Instagram</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Log In</a></li>
+                </ul>
+            </div>
+        </header>
+
+    )
+}
