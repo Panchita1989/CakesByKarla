b0298b7bd95df84d68163da8920eb008f5f07aaa
Panchita <franciscalandwehr@hotmail.com>
2025-05-12 14:01:00 -0500

    installed react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
 import './styles/App.css'
 import './styles/main.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import SlideInMenu from './components/SlideInMenu'
-import LandingPage from './pages/LandingPage'   
+import LandingPage from './pages/LandingPage'  
+import ChooseYourCake from './pages/ChooseYourCake' 
 
 function App() {
  return(
   <>
   <Header />
   <SlideInMenu />
-    <LandingPage />
+  <Routes>
+    <Route path="/" element={<LandingPage />} />
+    <Route path='/choose' element={<ChooseYourCake />} />
+  </Routes>
   <Footer />
   </>
  )
