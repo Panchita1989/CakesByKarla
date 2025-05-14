9fffbf7f3c063ed9735788a64e0b49a1e44eab33
Panchita <franciscalandwehr@hotmail.com>
2025-05-13 20:09:45 -0500

    changes in contact file

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,3 +1,10 @@
 export default function Contact() {
- 
+    return ( 
+        <div>
+            <article>
+                <h1>hello world</h1>
+            </article>
+        </div>
+    
+        )
 }
